0e5e62c13244be6457266bbd9bc2eeb164d242b9
Tuojing huang <a24364042@gmail.com>
2024-05-11 19:08:53 +0800

    add count

--- guess_num.py
+++ guess_num.py
@@ -5,9 +5,10 @@ while True:
 	count = count + 1
 	guess = int(input('請從1到100中輸入一個數字: '))
 	if guess == num:
-		print('終於猜對了!總共猜了', count, '次')
+		print('終於猜對了!這是你猜的第', count, '次')
 		break
 	elif guess > num:
 		print('比答案大')
 	elif guess < num: 
-		print('比答案小')	
+		print('比答案小')
+	print('這是你猜的第', count, '次')		
